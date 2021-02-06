e2913face38790402e47cbff172c37c5b3f51864
温馨佳园 <ijlhjj@126.com>
2021-02-06 20:51:55 +0800

    新增：Frame示例

---
+++ src/main/java/com/sweetmanor/demo/gui/awt/container/FrameDemo1.java
@@ -0,0 +1,31 @@
+package com.sweetmanor.demo.gui.awt.container;
+
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.TextField;
+
+/**
+ * Frame示例
+ * 
+ * @version 1.0 2014-08-26
+ * @author ijlhjj
+ */
+public class FrameDemo1 {
+
+	public static void main(String[] args) {
+		Frame frame = new Frame("Frame示例"); // Frame默认采用BorderLayout布局
+		System.out.println(frame.getLayout());
+
+		Panel panel = new Panel(); // Panel默认采用FlowLayout布局
+		System.out.println(panel.getLayout());
+
+		panel.add(new TextField(10));
+		panel.add(new Button("单击我"));
+		frame.add(panel);
+
+		frame.setBounds(400, 200, 400, 200);
+		frame.setVisible(true); // Frame默认可见性为false
+	}
+
+}
