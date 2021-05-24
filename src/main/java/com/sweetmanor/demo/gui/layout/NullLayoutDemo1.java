2ac0d69f4bb5ae84ff24102e15beede111f66202
温馨佳园 <ijlhjj@126.com>
2021-05-24 21:29:54 +0800

    新增：NullLayout示例

---
+++ src/main/java/com/sweetmanor/demo/gui/layout/NullLayoutDemo1.java
@@ -0,0 +1,35 @@
+package com.sweetmanor.demo.gui.layout;
+
+import java.awt.Button;
+
+import javax.swing.JFrame;
+
+import com.sweetmanor.utils.FrameUtil;
+
+/**
+ * NullLayout示例
+ * 
+ * @version 1.0 2014-08-26
+ * @author ijlhjj
+ */
+public class NullLayoutDemo1 {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("NullLayout示例");
+		frame.setLayout(null);// 绝对布局必须在添加组件前设置位置和大小（位置允许重叠）
+
+		Button bt1 = new Button("第一个按钮");
+		bt1.setBounds(50, 80, 100, 80);
+		frame.add(bt1);
+
+		Button bt2 = new Button("第二个按钮");
+		bt2.setBounds(100, 50, 150, 100);
+		frame.add(bt2);
+
+		frame.setSize(400, 300);
+		FrameUtil.center(frame);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
