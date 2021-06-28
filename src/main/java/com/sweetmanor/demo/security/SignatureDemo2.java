b2d0dd2567ea2a0b1fc32c3162211c42a4d3d03c
温馨佳园 <ijlhjj@126.com>
2021-06-28 21:03:48 +0800

    新增：使用非对称加密进行数字签名和验证示例

---
+++ src/main/java/com/sweetmanor/demo/security/SignatureDemo2.java
@@ -0,0 +1,44 @@
+package com.sweetmanor.demo.security;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+
+/**
+ * 使用DSA实现数字签名
+ * 
+ * @version 1.0 2016-06-16
+ * @author wenz
+ */
+public class SignatureDemo2 {
+
+	public static void main(String[] args) throws Exception {
+		String msg = "明天又是新的一天！";
+		System.out.println("原文是：" + msg);
+		byte[] msgBytes = msg.getBytes();
+
+		KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DSA");
+		keyPairGen.initialize(1024);
+		KeyPair keyPair = keyPairGen.generateKeyPair();// 生成密钥对
+
+		Signature sign = Signature.getInstance("DSA");// 指定用DSA创建数字签名对象
+		PrivateKey privateKey = keyPair.getPrivate();// 获取私钥，用私钥进行签名
+		sign.initSign(privateKey);// 用私钥初始化签名操作
+		sign.update(msgBytes);// 更新签名内容
+		byte[] signBytes = sign.sign();// 进行签名操作，得到结果数组
+		System.out.println("签名是：" + new String(signBytes));
+
+		PublicKey publicKey = keyPair.getPublic();// 获取公钥，用公钥进行验证
+		sign.initVerify(publicKey);// 用公钥初始化验证操作
+		sign.update(msgBytes);// 更新签名内容
+
+		if (sign.verify(signBytes)) {// 进行验证，输出验证信息
+			System.out.println("签名验证成功");
+		} else {
+			System.out.println("签名验证失败");
+		}
+	}
+
+}
